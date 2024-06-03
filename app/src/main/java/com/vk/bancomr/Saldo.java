b6d38f703b0c59a3a37bea8ad0785f0cfb600fd7
vk <pablogonzaga14@gmail.com>
2024-06-03 16:38:47 -0300

    Finalizado tela Saldo

--- app/src/main/java/com/vk/bancomr/Saldo.java
+++ app/src/main/java/com/vk/bancomr/Saldo.java
@@ -1,6 +1,8 @@
 package com.vk.bancomr;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +10,29 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Saldo extends AppCompatActivity {
+import com.vk.bancomr.databinding.ActivitySaldoBinding;
 
+public class Saldo extends AppCompatActivity {
+    private ActivitySaldoBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        binding = ActivitySaldoBinding.inflate(getLayoutInflater());
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_saldo);
+        setContentView(binding.getRoot());
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        getSupportActionBar().hide();
+
+        binding.toolbarSaldo.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent navegarTelaPrincipal = new Intent(Saldo.this,MainActivity.class);
+                startActivity(navegarTelaPrincipal);
+            }
         });
     }
 }
